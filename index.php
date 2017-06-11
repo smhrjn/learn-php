778a513069fa114abc4435fc4efaa1cf9ed033d9
saratmaharjan <sarat.maharjan@gmail.com>
2017-06-11 20:13:42 +0200

    next: laracast 10: functions

--- index.php
+++ index.php
@@ -2,7 +2,54 @@
 
 	$greeting = "from index";
 	$name = 'its me';
-	$names = ['name1', 'name2', 'name3'];
+
+	/*
+		// Array
+		$names = ['name1', 'name2', 'name3'];
+		$names[] = 'name4';
+		<ul>
+			<?php foreach ($names as $name) : ?>
+				<li> <?= $name ?> </li>
+			<?php endforeach; ?>
+		</ul>
+	*/
+
+	/*
+		// Associative Array
+		$person = [
+			'name' => 'someone',
+			'age' => 31,
+			'hair' => 'brown'
+		];
+		$person['career'] = 'web';
+		unset($person['age']);
+		echo '<pre>';
+		var_dump($person);
+		echo '</pre>';
+		<ul>
+			<?php foreach ($person as $key => $value) : ?>
+				<li> <?= $key ?> : <?= $value ?> </li>
+			<?php endforeach; ?>
+		</ul>
+	*/
+
+	/*
+		$task = [
+			'title' => 'do it',
+			'due' => 'today',
+			'completed' => true
+		];
+		<ul>
+			<li>
+				<strong>Name: </strong><?= ucwords($task['title']) ?>
+			</li>
+			<li>
+				<strong>Status: </strong><?= $task['completed'] ? 'completed' : 'incomplete' ?>
+			</li>
+		</ul>
+	*/
+
+	// die();
 
 	require 'index.view.php';
 
--- index.view.php
+++ index.view.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Learn php</title>
+	<title>php tutorials from Laravel</title>
 </head>
 <body>
 
-	<h1>Learning PHP</h1>
+	<h1>learn-php</h1>
 
 	<?php
 		echo "Hello " . "$greeting, $name";
@@ -13,13 +13,6 @@
 
 	<hr>
 
-	<ul>
-		<?php foreach ($names as $name) : ?>
-			<li> <?= $name ?> </li>
-		<?php endforeach; ?>
-	</ul>
-
 	<!--<?= htmlspecialchars($_GET['name']) ?>-->
-
 </body>
 </html>
